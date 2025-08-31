1ea034d11716c7f2a416348fc093181299753528
ibros1 <maxamedsuhayb41@gmail.com>
2025-08-31 14:18:58 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-    const root = window.document.documentElement;
+    const root = document.documentElement;
     if (isDarkMode) {
       root.classList.add("dark");
       localStorage.setItem("dark", "true");
