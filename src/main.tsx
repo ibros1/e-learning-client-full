7d1a9658049413f6b2d4b839046509ca7fd99250
ibros1 <maxamedsuhayb41@gmail.com>
2025-08-31 13:33:10 +0300

    added checkOut skeleton Style when loading

--- src/main.tsx
+++ src/main.tsx
@@ -4,19 +4,22 @@ import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { ThemeProvider } from "./components/theme-provider.tsx";
-import { CartProvider } from "../src/components/context/cartProvider.tsx";
+import { CartProvider } from "./components/context/cartProvider.tsx";
 import { store } from "./store/store";
 import { Toaster } from "react-hot-toast";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-    <StrictMode>
-      <CartProvider>
-        <Provider store={store}>
-          <App />
-          <Toaster />
-        </Provider>
-      </CartProvider>
-    </StrictMode>
-  </ThemeProvider>
+  <BrowserRouter>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <StrictMode>
+        <CartProvider>
+          <Provider store={store}>
+            <App />
+            <Toaster />
+          </Provider>
+        </CartProvider>
+      </StrictMode>
+    </ThemeProvider>
+  </BrowserRouter>
 );
